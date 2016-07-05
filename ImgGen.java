d62293b1a4df77a033b94639b46643ac302066a6
Shea Gosnell <gosnell_shea@hotmail.co.nz>
2016-07-05 15:57:55 +1200

    Added code to read the first line from standard in, and determine the maximum number of characters to expect on each line, and the number of lines to expect

--- ImgGen.java
+++ ImgGen.java
@@ -1,16 +1,39 @@
 import java.io.*;
 class ImgGen
 {
-  public static void main(String[] argv)
-    throws IOException
-  {
-	//reader to read from standard input
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	//read a line from standard input
-    String input = reader.readLine();
-	//parse as an integer.
-    int n = Integer.parseInt(input);
-	//writeto standard output
-    System.out.println(input);
+	public static void main(String[] argv)
+		throws IOException
+	{
+			//reader to read from standard input
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			//read first line from standard input
+			//Should be two integer numbers, separated by a space
+		String[] input = reader.readLine().split(" ");
+		  
+		  	//should be two inputs on this line
+		if(input.length != 2)
+		{
+			System.out.println("First line of input should be Length of longest line of text, and the number of lines to expect");
+			return;
+		}
+		
+		int width = 0;
+		int height = 0;
+			//parse the width and height for later use
+		try
+		{
+			width = Integer.parseInt(input[0]);
+			height = Integer.parseInt(input[1]);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Invalid number on line 0");
+			return;
+		}
+
+			//parse as an integer.
+		//int n = Integer.parseInt(input);
+			//write to standard output
+		//System.out.println(input);
   }
 }
