d9d2c4dba0fc2acac358980cc960478fb7f49bf1
Shea Gosnell <gosnell_shea@hotmail.co.nz>
2016-07-05 16:56:39 +1200

    Changed from throws declaration to try...catch blocks

--- ImgGen.java
+++ ImgGen.java
@@ -2,15 +2,24 @@ import java.io.*;
 class ImgGen
 {
 	public static void main(String[] argv)
-		throws IOException
 	{
 			//reader to read from standard input
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			//read first line from standard input
 			//Should be two integer numbers, separated by a space
-		String[] input = reader.readLine().split(" ");
-		  
-		  	//should be two inputs on this line
+		String[] input;
+		
+		try
+		{
+			input = reader.readLine().split(" ");
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error reading line 0");
+			return;
+		}  
+		
+			//should be two inputs on this line
 		if(input.length != 2)
 		{
 			System.out.println("First line of input should be Length of longest line of text, and the number of lines to expect");
@@ -31,15 +40,25 @@ class ImgGen
 			return;
 		}
 
-		//read height lines from standard input
+			//read height lines from standard input
 		for(int i = 1; i<=height; i++)
 		{
-			String line = reader.readLine();
+			String line;
+			
+			try
+			{
+				line = reader.readLine();
+			}
+			catch(IOException e)
+			{
+				System.out.println("Error reading line " + i);
+				return;
+			}
 			
 			if(line.length() > width)
 			{
 				System.out.println("Line " + i + " is too long, length is " + line.length() + " Should be no more than " + width);
-				return
+				return;
 			}
 			//to ensure that lines are read properly, echo the line to standard output
 			System.out.println(line);
